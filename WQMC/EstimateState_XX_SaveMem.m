4d29393a806f766c489351cdc8c8564485e22ce8
Shen Wang <shen.wang@utsa.edu>
2020-07-31 18:36:44 -0500

    Implement a new estimation function that saves memory a lot; put plot function into plotFigure folder; organize rule-based function to RuleBasedControl folder;

--- WQMC/EstimateState_XX_SaveMem.m
+++ WQMC/EstimateState_XX_SaveMem.m
@@ -1,4 +1,4 @@
-function [x_estimated,XX] = EstimateState_XX(CurrentValue,IndexInVar,aux,ElementCount,q_B,tInMin,C0,PreviousValue)
+function XX = EstimateState_XX_SaveMem(CurrentValue,IndexInVar,aux,ElementCount,q_B,tInMin,C0,PreviousValue)
 NumberofX = IndexInVar.NumberofX;
 MassEnergyMatrix = aux.MassEnergyMatrix;
 XX = [];
@@ -11,45 +11,54 @@ if(tInMin==0)
     x_estimated = x0;
     XX = x_estimated;
 else
-    PreviousDelta_t = PreviousValue.PreviousDelta_t;
+    Previous_delta_t = PreviousValue.PreviousDelta_t;
     PreviousSystemDynamicMatrix = PreviousValue.PreviousSystemDynamicMatrix;
-    X_estimated = PreviousValue.X_estimated;
+    x_estimated = PreviousValue.X_estimated;
     U_C_B_eachStep = PreviousValue.U_C_B_eachStep;
     [nodeCount,~] = size(U_C_B_eachStep);
     % We need to know the states 5 mins ago, apply the system dynamic for
     % the past 5 mins to obtain the estimation of current value.
     A = PreviousSystemDynamicMatrix.A;
     B = PreviousSystemDynamicMatrix.B;
-    C = PreviousSystemDynamicMatrix.C;
+    %    C = PreviousSystemDynamicMatrix.C;
     
     Hq_min = Constants4Concentration.Hq_min;
     
-    PreviousIndex = tInMin/Hq_min;
-    Previous_x = X_estimated(:,PreviousIndex);
-    Previous_delta_t = PreviousDelta_t(PreviousIndex);
-    
-    % how many steps in 5 mins
+    % how many steps in 1 min
     SetTimeParameter = Hq_min * Constants4Concentration.MinInSecond/Previous_delta_t;
     Np = round(SetTimeParameter);
-    %Np = floor(SetTimeParameter) + 1;
-    x_estimated = Previous_x;
     
     U = U_C_B_eachStep;
-    XX = [];
+    XX = zeros(NumberofX,Hq_min);
+    
+    %     A_1min = A^StepIn1Min;
+    %     A_1min = speye(NumberofX);
+    
+    %     for i = 1:StepIn1Min
+    %         A_1min =  A_1min * A;
+    %     end
+    
+    %     B_1min = sparse()
+    %     for i = 1:StepIn1Min
+    %         B_1min =
+    %     end
+    IndexofApplyingU = [];
+    for i = 1:Hq_min
+        IndexofApplyingU = [IndexofApplyingU round(i * Constants4Concentration.MinInSecond/Previous_delta_t)];
+    end
+    indexEachMin  = 1;
     for i = 1:Np
         if(aux.COMPARE == 1)
             x_estimated = A * x_estimated;
         else
             x_estimated = A * x_estimated + B * U(:,i);
+            % disp('to do');
         end
-        XX = [XX x_estimated];
+        if( i == IndexofApplyingU(indexEachMin))
+            XX(:,indexEachMin) = x_estimated;
+            indexEachMin = indexEachMin + 1;
+        end
+        
     end
-    IndexofApplyingU = [];
-    Hq_min = Constants4Concentration.Hq_min;
-    for i = 1:(Hq_min)
-        IndexofApplyingU = [IndexofApplyingU round(i * Constants4Concentration.MinInSecond/Previous_delta_t)];
-    end
-    % Save the value at each minute 
-    XX = XX(:,IndexofApplyingU);
 end
 end
